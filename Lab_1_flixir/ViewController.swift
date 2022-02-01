330b5b60bd52640dcbfb760a01459057b542e060
Mario Olivares <MarioOlivares_95@hotmail.com>
2022-02-01 11:30:30 -0500

    Initial Commit

---
+++ Lab_1_flixir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_1_flixir
+//
+//  Created by Mario Olivares on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
